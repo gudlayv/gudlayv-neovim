5fa2f61feb88a869ca1c18e5b576d816c5732f0d
gudlayv <rotachev.g@gmail.com>
2023-05-12 17:10:50 +0300

    init

---
+++ lua/core/mappings.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = " "
+
+-- NeoTree
+vim.keymap.set("n", "<leader>f", ":Neotree float focus toggle<CR>")
+vim.keymap.set("n", "<leader>E", ":Neotree focus % <CR>")
+vim.keymap.set("n", "<leader>e", ":Neotree focus toggle<CR>")
+vim.keymap.set("n", "<leader>o", ":Neotree float git_status<CR>")
+
+-- Navigation
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>/", ":CommentToggle<CR>")
+
+-- Splits
+vim.keymap.set("n", "|", ":vsplit<CR>")
+vim.keymap.set("n", "\\", ":split<CR>")
+
+-- Other
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+
+-- Tabs
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<leader>c", ":bdelete<CR>")
+vim.keymap.set("n", "<leader>bc", ":bdelete<CR>")
+vim.keymap.set("n", "<leader>bx", ":BufferLinePickClose<CR>")
+vim.keymap.set("n", "<leader>bC", ":BufferLineGroupClose ungrouped<CR>")
+
+-- Terminal
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
+vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,63 @@
+require("window-picker").setup({
+	autoselect_one = false,
+	include_current = false,
+	filter_rules = {
+		bo = {
+			filetype = { "neo-tree", "neo-tree-popup", "notify" },
+			buftype = { "terminal", "quickfix" },
+		},
+	},
+	other_win_hl_color = "#e35e4f",
+})
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+require("neo-tree").setup({
+	default_component_configs = {
+		icon = {
+			folder_empty = "󰜌",
+			folder_empty_open = "󰜌",
+		},
+		git_status = {
+			symbols = {
+				renamed = "󰁕",
+				unstaged = "󰄱",
+			},
+		},
+	},
+	document_symbols = {
+		kinds = {
+			File = { icon = "󰈙", hl = "Tag" },
+			Namespace = { icon = "󰌗", hl = "Include" },
+			Package = { icon = "󰏖", hl = "Label" },
+			Class = { icon = "󰌗", hl = "Include" },
+			Property = { icon = "󰆧", hl = "@property" },
+			Enum = { icon = "󰒻", hl = "@number" },
+			Function = { icon = "󰊕", hl = "Function" },
+			String = { icon = "󰀬", hl = "String" },
+			Number = { icon = "󰎠", hl = "Number" },
+			Array = { icon = "󰅪", hl = "Type" },
+			Object = { icon = "󰅩", hl = "Type" },
+			Key = { icon = "󰌋", hl = "" },
+			Struct = { icon = "󰌗", hl = "Type" },
+			Operator = { icon = "󰆕", hl = "Operator" },
+			TypeParameter = { icon = "󰊄", hl = "Type" },
+			StaticMethod = { icon = "󰠄 ", hl = "Function" },
+		},
+	},
+	filesystem = {
+		filtered_items = {
+			visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+			hide_dotfiles = false,
+			hide_gitignored = true,
+		},
+		follow_current_file = true, -- This will find and focus the file in the active buffer every
+	},
+	buffers = {
+		follow_current_file = true, -- This will find and focus the file in the active buffer every
+	},
+})
