99b9e51563e26c753467f864a72832bb2f5ea12d
gudlayv <g.rotachev@gmail.com>
2023-06-21 16:54:33 +0300

    Added go to type definition

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -18,14 +18,16 @@ saga.setup({
 
 local diagnostic = require("lspsaga.diagnostic")
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
-vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
--- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-vim.keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
-vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+local keymap = vim.keymap
+keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
+keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
+keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
+keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
+keymap.set("n", "gT", "<cmd>Lspsaga goto_type_definition<CR>")
 
 -- code action
 vim.keymap.set({ "n", "v" }, "<leader>la", "<cmd>Lspsaga code_action<CR>")
